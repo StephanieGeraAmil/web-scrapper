9b6d97bbb965286be79eccc7ec636abdf468ce05
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2021-10-12 20:46:12 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const PORT=500;
+const express= require ('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const app= express();
+app.listen(PORT,()=>console.log(`server running on port: ${PORT}`));
